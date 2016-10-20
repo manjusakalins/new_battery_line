ec34649a17a95c6ae650a74984a591aa154a410a
linhuasheng <linhuasheng@jlinksz.com>
2016-10-20 09:51:47 +0800

    update_read_lk_to_gen_ocv_file

--- gen_battery_line_from_uart_log/read_lk_to_gen_ocv.py
+++ gen_battery_line_from_uart_log/read_lk_to_gen_ocv.py
@@ -47,7 +47,18 @@ sum_q=0;#mean vbat 3.45v
 real_sum_q=0; #3.2v?
 stop_sumq=0;
 real_rec_num=0;
+g_customer="customer"
+g_vendor="vendor"
+g_max_q = 3000;
+l_idx=0;
 for cur_line in lines:
+    if l_idx == 1:
+        g_customer = cur_line.strip();
+    if l_idx == 2:
+        g_vendor = cur_line.strip();
+    if l_idx == 3:
+        g_max_q = int(cur_line.strip());
+    l_idx = l_idx + 1;
     if cur_line.find('jlink_show_done_record') != -1:
         print cur_line
         record_start=1;
@@ -92,7 +103,9 @@ for cur_line in lines:
 print sum_q
 print len(top_rec)
 print real_rec_num;
-
+print g_customer
+print g_vendor
+print g_max_q
 
 
 def jlink_print_ocv_file(max_q, g_print_h, customer, vendor):
@@ -121,8 +134,8 @@ def jlink_print_ocv_file(max_q, g_print_h, customer, vendor):
         out_ocv = "%sbattery_profile_t2_num = <77>;\nbattery_profile_t2 = <0 %d\n" % (dtsi_start, top_rec[0]["ocv"])
         out_r = "r_profile_t2_num = <77 >;\nr_profile_t2 = <0 %d\n" % top_rec[0]["ocv"]
     else:
-        out_ocv="//start define\nBATTERY_PROFILE_STRUC lk_py_%s_%s_ocv_%dmah[] =\n{\n{0, %d},\n" % (customer, vendor, max_q,top_rec[0]["ocv"]);
-        out_r="R_PROFILE_STRUC lk_py_%s_%s_r_%dmah[] =\n{\n{0, %d},\n" % (customer, vendor, max_q,top_rec[0]["ocv"]);
+        out_ocv="//start define\nBATTERY_PROFILE_STRUC lk_py_%s_%s_ocv_%dmah[] =\n{\n{0, %d},\n" % (g_customer, g_vendor, g_max_q,top_rec[0]["ocv"]);
+        out_r="R_PROFILE_STRUC lk_py_%s_%s_r_%dmah[] =\n{\n{0, %d},\n" % (g_customer, g_vendor, g_max_q,top_rec[0]["ocv"]);
 
 
     for idx in range(1,77):
@@ -169,13 +182,95 @@ def jlink_print_ocv_file(max_q, g_print_h, customer, vendor):
     #print out_r
 
     if g_print_h == 0:
-        out_file=input_dir + "/lk_zcv_%s_%s_%dmah.dtsi" % (customer, vendor, max_q)
+        out_file=input_dir + "/lk_zcv_%s_%s_%dmah.dtsi" % (g_customer, g_vendor, g_max_q)
     else:
-        out_file=input_dir + "/lk_zcv_%s_%s_%dmah.h" % (customer, vendor, max_q)
+        out_file=input_dir + "/lk_zcv_%s_%s_%dmah.h" % (g_customer, g_vendor, g_max_q)
     genf=open(out_file, 'w+');
     genf.write(out_ocv);
     genf.write(out_r);
     genf.close();
 
-jlink_print_ocv_file(4000, 0, "menshidai", "hxny");
-jlink_print_ocv_file(4000, 1, "menshidai", "hxny");
+def jlink_print_ocv_file_sprd(max_q, g_print_h, customer, vendor):
+    global sum_q;
+    real_max_q = sum_q;#10mah
+    #print top_rec
+    #cur_sums_q = top_rec[0]["cur"] * top_rec[0]["time"] / 36000;
+    cur_sums_q = top_rec[0]["dis_q"]
+
+#	ocv-tab-vol = <4337 4273 4217 4163 4111 4067 4008 3973 3935 3884 3849 3826 3807 3792 3779 3764 3745 3721 3691 3682 3400>;
+#	ocv-tab-cap = <100  95   90   85   80   75   70   65   60   55   50   45   40   35   30   25   20   15   10   5    0>;
+    dtsi_start='''/ {
+	//start
+		sprd_battery_x: sprd_battery_x {
+		/* battery: %s %s %dmah real: %dmah*/
+		compatible = "sprd,sprd-battery-cust-x";
+		num_table = <1 >;
+    ''' % (g_customer, g_vendor,g_max_q, real_max_q/10)
+   
+    out_ocv ="\tocv-tab-vol = <%d " % (top_rec[0]["ocv"])
+    out_cap = "\tocv-tab-cap = <100 ";
+    out_r_ocv = "\trint-tab-ocv = <%d " % (top_rec[0]["ocv"])
+    out_r_rint = "\trint-tab-rint = <0 ";
+
+
+    
+    for idx in range(1,77):
+        if idx < len(top_rec) :
+            print idx
+            #print top_rec[idx-1]["size"] - 1
+
+            r = (top_rec[idx]["ocv"] - int(top_rec[idx-1]["vbats"][top_rec[idx-1]["size"] - 1]))*10000/top_rec[idx-1]["cur"]
+            dq = top_rec[idx]["dis_q"];
+
+            if g_print_h == 0:
+                out_ocv="%s%d " % (out_ocv, top_rec[idx]["ocv"])
+                out_cap="%s%d " % (out_cap, 100-(cur_sums_q*1000/real_max_q + 5)/10)
+
+                out_r_ocv="%s%d " % (out_r_ocv, top_rec[idx]["ocv"])
+                out_r_rint="%s%d " % (out_r_rint, r)
+
+            else:
+                out_ocv="%s{%d, %d},\n" % (out_ocv, (cur_sums_q*1000/real_max_q + 5)/10, top_rec[idx]["ocv"])
+
+            cur_sums_q = cur_sums_q + dq;
+            print cur_sums_q, real_max_q
+            if cur_sums_q > real_max_q:
+                break;
+    print idx
+    out_head = "%s\tocv-tab-size = <%d>;\n\trint-tab-size = <%d>;\n" % (dtsi_start, idx+2, idx+2)
+
+    if g_print_h == 0:
+        out_ocv = "%s>;\n" % out_ocv
+        out_cap = "%s>;\n" % out_cap
+        
+        out_r_ocv = "%s>;\n" % out_r_ocv
+        out_r_rint = "%s>;\n" % out_r_rint
+    else:
+        out_ocv="%s};\n" % out_ocv;
+        out_r="%s};\n" % out_r;
+
+    #print out_ocv
+    #print out_r
+
+    if g_print_h == 0:
+        out_file=input_dir + "/lk_zcv_%s_%s_%dmah_sprd.dtsi" % (g_customer, g_vendor, g_max_q)
+    else:
+        out_file=input_dir + "/lk_zcv_%s_%s_%dmah.h" % (g_customer, g_vendor, g_max_q)
+    genf=open(out_file, 'w+');
+    genf.write(out_head);
+    genf.write(out_ocv);
+    genf.write(out_cap);
+    genf.write(out_r_ocv);
+    genf.write(out_r_rint);
+    genf.close();
+
+
+#jlink_print_ocv_file(3200, 0, "lingyun", "wls");
+#jlink_print_ocv_file(3200, 1, "lingyun", "wls");
+#jlink_print_ocv_file_sprd(3200, 0, "lingyun", "wls");
+
+# we need customer in input line2, vendor in line3, max_q in line4.
+
+jlink_print_ocv_file(3200, 0, "c", "v");
+jlink_print_ocv_file(3200, 1, "c", "v");
+jlink_print_ocv_file_sprd(3200, 0, "c", "v");
